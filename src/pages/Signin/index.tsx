860e4b43e9bef9ea1af5eb861314b2a7a0725340
Arllain <arllain@gmail.com>
2020-12-26 13:28:17 -0300

    Unform library #5

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,31 +1,38 @@
 import React from 'react';
+import { Form } from '@unform/web';
 import { FiLogIn, FiUserPlus, FiLock } from 'react-icons/fi';
 import { Container, Content, BackGround } from './styles';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-const SignIn: React.FC = () => (
-  <Container>
-    <Content>
-      <form>
-        <h1>Sign In</h1>
-        <Input name="login" icon={FiLogIn} placeholder="Login" />
-        <Input
-          name="password"
-          icon={FiLock}
-          type="password"
-          placeholder="Password"
-        />
-        <Button type="submit">Sign In now</Button>
-      </form>
+const SignIn: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
 
-      <a href="signup">
-        <FiUserPlus />
-        Sign Up
-      </a>
-    </Content>
-    <BackGround />
-  </Container>
-);
+  return (
+    <Container>
+      <Content>
+        <Form onSubmit={handleSubmit}>
+          <h1>Sign In</h1>
+          <Input name="login" icon={FiLogIn} placeholder="Login" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Password"
+          />
+          <Button type="submit">Sign In now</Button>
+        </Form>
+
+        <a href="signup">
+          <FiUserPlus />
+          Sign Up
+        </a>
+      </Content>
+      <BackGround />
+    </Container>
+  );
+};
 
 export default SignIn;
