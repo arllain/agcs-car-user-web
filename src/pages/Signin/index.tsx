1cc3951b0ab35bd8ca3d6af86de2d531c62c14e4
Arllain <arllain@gmail.com>
2020-12-26 16:37:57 -0300

    Fields validation #6

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,21 +1,45 @@
-import React from 'react';
-import { Form } from '@unform/web';
+import React, { useCallback, useRef } from 'react';
 import { FiLogIn, FiUserPlus, FiLock } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
+import getValidationErrors from '../../utils/getValidationErrors';
 import { Container, Content, BackGround } from './styles';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 const SignIn: React.FC = () => {
-  function handleSubmit(data: object): void {
-    console.log(data);
-  }
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        login: Yup.string()
+          .required('Login is required')
+          .min(5, 'Minimum password length: 5 characters'),
+        password: Yup.string().min(5, 'Minimum password length: 6 characters'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(err);
+        formRef.current?.setErrors(errors);
+      }
+    }
+  }, []);
 
   return (
     <Container>
       <Content>
-        <Form onSubmit={handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Sign In</h1>
           <Input name="login" icon={FiLogIn} placeholder="Login" />
+
           <Input
             name="password"
             icon={FiLock}
--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
 import {
   FiArrowLeft,
   FiMail,
@@ -8,37 +11,77 @@ import {
   FiLogIn,
   FiCalendar,
 } from 'react-icons/fi';
+import getValidationErrors from '../../utils/getValidationErrors';
 import { Container, Content, BackGround } from './styles';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-const SignUp: React.FC = () => (
-  <Container>
-    <BackGround />
-    <Content>
-      <form>
-        <h1>Sign Up</h1>
-        <Input name="firstName" icon={FiUser} placeholder="First Name" />
-        <Input name="lastName" icon={FiUser} placeholder="Last Name" />
-        <Input name="email" icon={FiMail} placeholder="E-mail" />
-        <Input name="birthday" icon={FiCalendar} placeholder="Birthday" />
-        <Input name="login" icon={FiLogIn} placeholder="Login" />
-        <Input
-          name="password"
-          icon={FiLock}
-          type="password"
-          placeholder="Password"
-        />
-        <Input name="phone" icon={FiPhone} placeholder="Phone Number" />
-        <Button type="submit">Sign Up now</Button>
-      </form>
+const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
 
-      <a href="criar">
-        <FiArrowLeft />
-        Back to Sign In
-      </a>
-    </Content>
-  </Container>
-);
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        firstName: Yup.string()
+          .required('First name is required')
+          .max(30, 'Maximum first name length: 30 characters'),
+        lastName: Yup.string()
+          .required('Last name is required')
+          .max(50, 'Maximum last name length: 50 characters'),
+        email: Yup.string()
+          .email('Enter a valida E-mail')
+          .required('E-mail is required'),
+        birthday: Yup.string().required('BirthDate  is requirer'),
+        login: Yup.string()
+          .required('Login  is required')
+          .min(5, 'Minimum login length: 5 characters'),
+        password: Yup.string().min(5, 'Minimum password length: 6 characters'),
+        phone: Yup.string()
+          .required('Phone  is required')
+          .min(9, 'Minimum phone length: 9 characters'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(err);
+        formRef.current?.setErrors(errors);
+      }
+    }
+  }, []);
+
+  return (
+    <Container>
+      <BackGround />
+      <Content>
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <h1>Sign Up</h1>
+          <Input name="firstName" icon={FiUser} placeholder="First Name" />
+          <Input name="lastName" icon={FiUser} placeholder="Last Name" />
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input name="birthday" icon={FiCalendar} placeholder="Birthday" />
+          <Input name="login" icon={FiLogIn} placeholder="Login" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Password"
+          />
+          <Input name="phone" icon={FiPhone} placeholder="Phone Number" />
+          <Button type="submit">Sign Up now</Button>
+        </Form>
+
+        <a href="criar">
+          <FiArrowLeft />
+          Back to Sign In
+        </a>
+      </Content>
+    </Container>
+  );
+};
 
 export default SignUp;
